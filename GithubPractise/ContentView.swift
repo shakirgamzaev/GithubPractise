e4a6819ea30fd9f5512b5dcb99b0460e15c868a4
Shakir Gamzaev <shkir100@gmail.com>
2024-12-24 17:27:11 +0100

    Initial Commit

---
+++ GithubPractise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GithubPractise
+//
+//  Created by shakir Gamzaev on 24/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GithubPractise/GithubPractiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubPractiseApp.swift
+//  GithubPractise
+//
+//  Created by shakir Gamzaev on 24/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubPractiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
