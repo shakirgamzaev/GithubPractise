63a87ccccd898cb6c2b015dbd93021e6bd91a36b
Shakir Gamzaev <shkir100@gmail.com>
2024-12-24 18:12:24 +0100

    Create HomeView with 3 Texts inside of it

---
+++ GithubPractise/HomeView.swift
@@ -0,0 +1,23 @@
+//
+//  HomeView.swift
+//  GithubPractise
+//
+//  Created by shakir Gamzaev on 24/12/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("Screen 1!")
+            Text("Screen 2!")
+            Text("Screen 3!")
+        }
+        .font(.largeTitle)
+    }
+}
+
+#Preview {
+    HomeView()
+}
