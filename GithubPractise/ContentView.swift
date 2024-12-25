185c932690ebe82b6132917120387b3538060153
Shakir Gamzaev <shkir100@gmail.com>
2024-12-25 20:21:19 +0100

    [Feature] change icon to house.fill, was heart.fill

--- GithubPractise/ContentView.swift
+++ GithubPractise/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack(spacing: 15.0) {
-            Image(systemName: "heart.fill")
+            Image(systemName: "house.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             
