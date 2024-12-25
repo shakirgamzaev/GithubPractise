44616cc7ea48c237642129e7f769af21602a2136
Shakir Gamzaev <shkir100@gmail.com>
2024-12-25 12:30:44 +0100

    Add VStack and a Text to see the width of entire display

--- GithubPractise/Testing.swift
+++ GithubPractise/Testing.swift
@@ -9,20 +9,36 @@ struct Testing: View {
     let items: [GridItem] = [
         GridItem(.adaptive(minimum: 130, maximum: .infinity), spacing: 10)
     ]
+    @State private var widthOfScreen = CGFloat.zero
     
     var body: some View {
         ScrollView {
-            LazyVGrid(columns: items) {
-                ForEach(0..<100) { _ in
-                    Rectangle()
-                        .fill(.orange.gradient)
-                        .frame(height: 80)
+            VStack {
+                Text("Width: \(String(format: "%.f", widthOfScreen))")
+                LazyVGrid(columns: items) {
+                    ForEach(0..<100) { _ in
+                        Rectangle()
+                            .fill(.orange.gradient)
+                            .frame(height: 80)
+                            .onGeometryChange(for: CGFloat.self) { geo in
+                                geo.size.width
+                            } action: { newValue in
+                                print(String(format: "%.2f", newValue))
+                            }
+
+                    }
                 }
             }
         }
         .scrollIndicators(.hidden)
         .clipShape(Rectangle())
         .ignoresSafeArea(edges: .bottom)
+        .onGeometryChange(for: CGFloat.self) { geo in
+            geo.size.width
+        } action: { newValue in
+            widthOfScreen = newValue
+        }
+
     }
 }
 
