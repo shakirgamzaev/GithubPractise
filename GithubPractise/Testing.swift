a0603cf03dae7872825ff2aed3458519f44fffbb
Shakir Gamzaev <shkir100@gmail.com>
2024-12-25 12:16:18 +0100

    Create LazyVGrid of Rectangles in Testing file

--- GithubPractise/Testing.swift
+++ GithubPractise/Testing.swift
@@ -6,8 +6,23 @@ import SwiftUI
 
 
 struct Testing: View {
+    let items: [GridItem] = [
+        GridItem(.adaptive(minimum: 130, maximum: .infinity), spacing: 10)
+    ]
+    
     var body: some View {
-        Text("Hello")
+        ScrollView {
+            LazyVGrid(columns: items) {
+                ForEach(0..<100) { _ in
+                    Rectangle()
+                        .fill(.orange.gradient)
+                        .frame(height: 80)
+                }
+            }
+        }
+        .scrollIndicators(.hidden)
+        .clipShape(Rectangle())
+        .ignoresSafeArea(edges: .bottom)
     }
 }
 
